01b59ed7c6719f755e8fdcdf03596e0bfd8f276e
Urian <uchang@sightmachine.com>
2018-10-20 14:19:35 -0700

    Updates

---
+++ precache-manifest.b1aff7b38406f5500927f3b205bdf723.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6781925f27b3af43ba22f117689c3a8d",
+    "url": "/woof/static/media/pf_icon.6781925f.png"
+  },
+  {
+    "revision": "581ca55e56994d3925b85cf76aa3f8c5",
+    "url": "/woof/static/media/pf_icon.581ca55e.png"
+  },
+  {
+    "revision": "ada30dd9ac8a34d9e448321b8a73bfdf",
+    "url": "/woof/static/media/dog-cover.ada30dd9.jpg"
+  },
+  {
+    "revision": "d38e75a6c3f64f9e1f39",
+    "url": "/woof/static/js/runtime~main.d38e75a6.js"
+  },
+  {
+    "revision": "85f5d18afc2afab59130",
+    "url": "/woof/static/js/main.85f5d18a.chunk.js"
+  },
+  {
+    "revision": "95e3711e017a470df7a2",
+    "url": "/woof/static/js/1.95e3711e.chunk.js"
+  },
+  {
+    "revision": "85f5d18afc2afab59130",
+    "url": "/woof/static/css/main.16a5b50f.chunk.css"
+  },
+  {
+    "revision": "1b0e1469014aa78ea4c7c66fe7bab0f2",
+    "url": "/woof/index.html"
+  }
+];
